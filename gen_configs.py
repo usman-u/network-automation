8584a8c6749df002b83ec0ecb2bf5fcfc0a7fd89
usman-umer <unumer@hotmail.com>
2022-03-25 17:54:34 +0000

    updated class name, from GenVyos to Vyos

--- gen_configs.py
+++ gen_configs.py
@@ -6,7 +6,7 @@ with open("routers.yml") as file:
 
 for device in raw["routers"]:
     print ("----", device["name"],"----")
-    # print (GenVyos.gen_int(device["interfaces"]))
-    # print (GenVyos.gen_bgp_peer(device["bgp_peers"], device["bgpasn"]))
 
-    print (GenVyos.gen_ospf_networks(device["ospf_networks"]))
+    print (Vyos.gen_int(device["interfaces"]))
+    print (Vyos.gen_bgp_peer(device["bgp_peers"], device["bgpasn"]))
+    print (Vyos.gen_ospf_networks(device["ospf_networks"]))
